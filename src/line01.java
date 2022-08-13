9976649685664ea33a2e1cea92675c1dde8a56da
Kunal <moralkun02@gmail.com>
2022-08-13 13:02:06 +0530

    Please don't share with anyone... vse chaye kitne bhe maaje kaaro... best of luck...

---
+++ src/line01.java
@@ -0,0 +1,39 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class line01 {
+    public static void main(String[] args) {
+
+        String a = "Moral     ";
+        System.out.println(a);
+        int L = a.length();
+        System.out.println(L);
+
+       /* String lcase = a.toLowerCase();
+        System.out.println(lcase);
+
+        String ucase = a.toUpperCase();
+        System.out.println(ucase);
+
+        String cut = a.trim();
+        System.out.println(cut);
+
+        String subS = a.substring(1);
+        System.out.println(subS);
+
+        String replace = a.replace('r','l');
+        System.out.println(replace);
+
+        String replace2 = a.replace("Moral","     ");
+        System.out.println(replace2);
+
+        String startC = String.valueOf(a.startsWith("l"));
+        System.out.println(startC);
+        String endC = String.valueOf(a.endsWith("mo"));
+        System.out.println(endC);*/
+
+        System.out.println(a.charAt(2));
+        System.out.println(a.indexOf());
+
+    }
+}
---
+++ src/top4.java
@@ -0,0 +1,12 @@
+public class top4 {
+    public static void main(String[] args) {
+        //increment operator
+        int a = 7;
+        int b = ++a*8;
+
+        System.out.println(b);
+        char x = 'q';
+        System.out.println(++x);
+
+    }
+}
